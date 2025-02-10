bbd3319a414a38501c0f3b1a33394c8f24c581a3
Pasindu Rasanjana <pasindurasanjana457825@gmail.com>
2025-02-10 20:22:54 +0530

    issue fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/pasindu/HomePage';
+import LoginForm from './pages/pasindu/LoginForm';
 
-function App() {
-    return <h1>Hello, React!</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import App from "./App"; // Make sure the path is correct
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
