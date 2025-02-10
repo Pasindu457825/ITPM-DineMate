b7fe667ed7f2818337158d87db4bea122a7cfc90
Pasindu Rasanjana <pasindurasanjana457825@gmail.com>
2025-02-11 01:44:58 +0530

    error fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/pasindu/HomePage';
-import LoginForm from './pages/pasindu/LoginForm';
 import AddItemForm from './pages/pasindu/AddItemForm';  // Import the AddItemForm component
 
 const App = () => {
@@ -9,7 +8,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginForm />} />
         <Route path="/add-item" element={<AddItemForm />} />  {/* Add a route for the AddItemForm */}
       </Routes>
     </Router>
