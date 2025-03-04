025ad6f998b972428437bccef4d157b1a00cd462
Tharusha <156021977+CodeMaestro2001@users.noreply.github.com>
2025-03-05 00:03:58 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors"); // Import CORS middleware
-const orderRoutes = require("./routes/pasindu/orderRoutes");
-const reservationRoutes = require("./routes/pasindu/reservationRoutes");
+const cors = require("cors");
 const dotenv = require("dotenv");
 
+// Import your routes
+
+//pasindu
+const orderRoutes = require("./routes/pasindu/orderRoutes");
+const reservationRoutes = require("./routes/pasindu/reservationRoutes");
+
+//tharusha
+const userRoutes = require("./routes/tharusha/userRoute");
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors()); // Enable CORS for all origins
+app.use(cors());
 
 // MongoDB connection
 mongoose
@@ -20,9 +28,11 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-// Use the routes
+// Route definitions
 app.use("/api/ITPM/orders", orderRoutes);
 app.use("/api/ITPM/reservations", reservationRoutes);
+// Add your user management route
+app.use("/api/ITPM/users", userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
